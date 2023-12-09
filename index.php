39e5e58272bffe4c59847187575aef55d4515dbc
Artyom <adavydovvv@mail.ru>
2023-12-09 17:07:25 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+<meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ТепGlove</title>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/css/bootstrap.min.css" rel="stylesheet">
+    <link href="bootstrap.min.css" rel="stylesheet">
+    <title>Давыдов Артём Сергеевич 221-362 - Лабораторная работа №11</title>
+</head>
+
+<body>
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-12 col-md-6 col-lg-4">
+        <h1>ТепGlove</h1>
+        <p>Перчатки с подогревом «ТепGlove» – ваше оружие против холода 🔥</p>
+      </div>
+      <div class="col-12 col-md-6 col-lg-4">
+        <img src="image.jpg" alt="Изображение проекта" class="img-fluid">
+      </div>
+    </div>
+  </div>
+
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-12 col-md-6 col-lg-4">
+        <h2>Основные особенности</h2>
+      </div>
+      <div class="col-12 col-md-6 col-lg-4">
+      <ul>
+          <li>ТепGlove - высококачественные зимние перчатки, которые одинаково хорошо подойдут как для города, так и для активного отдыха.</li>
+          <li>Структура перчаток сама по себе создаёт мощную защиту против холода, но если мороз крепчает, и барьер начинает уступать, то на помощь придёт подогрев!</li>
+          <li>У холода не останется шансов, а ваши руки получат необходимую порцию теплоты!</li>
+        </ul>
+      </div>
+    </div>
+  </div>
+
+  <div class="container mt-5">
+    <div class="row">
+      <div class="col-12 col-md-6 col-lg-4">
+        <h2>Свяжитесь с нами</h2>
+      </div>
+      <div class="col-12 col-md-6 col-lg-4">
+      <form>
+          <div class="mb-3">
+            <label for="name" class="form-label">Ваше имя</label>
+            <input type="text" class="form-control" id="name" required>
+          </div>
+          <div class="mb-3">
+            <label for="email" class="form-label">Email</label>
+            <input type="email" class="form-control" id="email" required>
+          </div>
+          <div class="mb-3">
+            <label for="message" class="form-label">Сообщение</label>
+            <textarea class="form-control" id="message" rows="3" required></textarea>
+          </div>
+          <button type="submit" class="btn btn-primary">Отправить</button>
+          <div class="mt-3">
+          <a href="https://t.me/tepglove" class="btn btn-primary me-2" target="_blank">Telegram</a>
+        </div>
+        </form>
+      </div>
+    </div>
+  </div>
+
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/5.3.0/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
